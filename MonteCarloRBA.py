4a18b4d005b13fead0b598f7c9dd6ae5ceca6b8c
LinusTC <tongchun03@gmail.com>
2025-03-20 12:16:52 +0000

    sure

--- MonteCarloRBA.py
+++ MonteCarloRBA.py
@@ -5,23 +5,30 @@ from PortfolioFunction import minimize_volatility, maximize_sharpe
 def MonteCarloRBA(ticker, covariances, returns, num_iterations=10000, max_on="sharpe", min_assets = 3, max_assets = 5, min_weight=0, max_weight=1):
     all_portfolios = []
     dominant_portfolios = []
+    frequency = {key: 1 for key in ticker}  # Initialize frequency of each ticker with 1 to avoid division by zero
 
     for _ in tqdm(range(num_iterations)):
         num_assets = np.random.randint(min_assets, max_assets) if min_assets != max_assets else min_assets
-        rand_assets = np.random.choice(list(ticker), num_assets, replace=False)
 
-        # Extract data for current portflio
+        # Adjust selection probability based on inverse frequency
+        total_frequency = sum(1.0 / frequency[t] for t in ticker)
+        probabilities = [(1.0 / frequency[t]) / total_frequency for t in ticker]
+        rand_assets = np.random.choice(list(ticker), num_assets, replace=False, p=probabilities)
+
+        # Update frequency of selected assets
+        for asset in rand_assets:
+            frequency[asset] += 1
+
+        # Extract data for current portfolio
         selected_returns = returns.loc[rand_assets].values
         selected_covariances = covariances.loc[rand_assets, rand_assets].values
 
-        # Optimize weights of each portfolio based on Sharpe ratio
-        if max_on=="vol":
+        # Optimize weights of each portfolio based on selected metric
+        if max_on == "vol":
             asset_weights = minimize_volatility(selected_returns, selected_covariances)
-
-        if max_on=="random":
+        elif max_on == "random":
             weights = np.random.rand(num_assets)
-            asset_weights = weights/sum(weights)
-
+            asset_weights = weights / sum(weights)
         else:
             asset_weights = maximize_sharpe(selected_returns, selected_covariances, 0, min_weight, max_weight)
 
@@ -40,7 +47,6 @@ def MonteCarloRBA(ticker, covariances, returns, num_iterations=10000, max_on="sh
 
         # Check if is dominated
         is_dominated = any(portfolio["return"] >= curr_portfolio_returns and portfolio["variance"] <= curr_portfolio_var for portfolio in dominant_portfolios)
-
         if not is_dominated:
             dominant_portfolios.append(portfolio_data)
 
